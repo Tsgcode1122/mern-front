74dbe1eb674b0eed7cde122451d88bf2b8364ce3
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2024-03-17 12:15:14 +0100

    frontend

---
+++ src/components/Loader.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState, CSSProperties } from "react";
+import HashLoader from "react-spinners/HashLoader";
+const Loader = () => {
+  let [loading, setLoading] = useState(true);
+  let [color, setColor] = useState("#ffffff");
+
+  return (
+    <div>
+      <div className="sweet-loading text-center">
+        <HashLoader color="#0000" loading={loading} size={80} />
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
